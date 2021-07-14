c2a5e51897ea19f2e78d2bca3cd7bd2e3aa76e07
ArtSpd <76838598+ArtSpd@users.noreply.github.com>
2021-07-14 14:14:12 +0300

    Initial Commit

---
+++ Project 22 iBeacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 22 iBeacon
+//
+//  Created by Артем Волков on 14.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
